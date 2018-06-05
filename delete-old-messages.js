23ac8a55b3533ccf9875ba8cf23aa7fb8be4fd46
arpit <arpit.tarang@gmail.com>
2018-06-05 21:49:16 +0530

    added delete messages

---
+++ delete-old-messages.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+var moment = require("moment");
+var mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/test-twitch-app"
+);
+var MessageCount = require("./messagecount");
+
+// delete all message that are older than one hour and not tweeted
+(async () => {
+  await MessageCount.deleteMany({
+    tweeted: false,
+    lastUpdated: {
+      $lt: moment()
+        .add(-12, "hours")
+        .toDate()
+    }
+  });
+  process.exit();
+})();
