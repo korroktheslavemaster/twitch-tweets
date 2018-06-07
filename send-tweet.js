51fc3b5e8f1fcf4905386b7a745538db55673ca5
arpit <arpit.tarang@gmail.com>
2018-06-07 19:15:49 +0530

    fixed util bug

--- send-tweet.js
+++ send-tweet.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
+var util = require("util");
 var tweet = require("./twitter-bot");
 var moment = require("moment");
 var ss = require("string-similarity");
