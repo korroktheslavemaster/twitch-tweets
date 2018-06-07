79b7cb631a0472e04106120f0c76203ac7309001
arpit <arpit.tarang@gmail.com>
2018-06-07 12:59:37 -0500

    added get-clusters.js

---
+++ get-clusters.js
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+var moment = require("moment");
+var fs = require("fs");
+
+module.exports = async messages => {
+  fs.writeFileSync(
+    "/tmp/messagecount-recent.json",
+    JSON.stringify(messages),
+    "utf8"
+  );
+  require("child_process").execSync(
+    "python get-cluster-tf.py /tmp/messagecount-recent.json /tmp/output.json > /dev/null"
+  );
+  var clusters = require("/tmp/output.json");
+  return clusters;
+};
---
+++ test/get-clusters.js
@@ -0,0 +1,56 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+var mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/test-twitch-app"
+);
+var MessageCount = require("../messagecount");
+var moment = require("moment");
+var fs = require("fs");
+var getClusters = require("../get-clusters");
+// var messages = require("/tmp/messagecount-recent.json");
+
+// messages = messages.sort((a, b) => -a.count + b.count);
+
+// var main = async () => {
+//   // filter 1 counts. actually don't, they are useful eg. r9k mode
+//   // used up elements
+//   var done = new Array(messages.length).map(x => false);
+//   var groupings = {};
+//   for (const [idx, message] of messages.entries()) {
+//     if (done[idx]) continue;
+//     done[idx] = true;
+//     var id = message._id;
+//     groupings[id] = {
+//       messages: [message.message],
+//       count: message.count
+//     };
+//     messages.forEach((val, idx2) => {
+//       if (done[idx2]) return;
+//       if (ss.compareTwoStrings(message.message, val.message) > 0.9) {
+//         // same
+//         groupings[id].count += val.count;
+//         groupings[id].messages.push(val.message);
+//         done[idx2] = true;
+//       }
+//     });
+//   }
+//   console.log(groupings);
+// };
+
+// main();
+
+var main = async () => {
+  var messages = await MessageCount.find({
+    tweeted: false,
+    lastUpdated: {
+      $gt: moment()
+        .add(-30, "minutes")
+        .toDate()
+    }
+  });
+  console.log(await getClusters(messages));
+};
+
+main();
